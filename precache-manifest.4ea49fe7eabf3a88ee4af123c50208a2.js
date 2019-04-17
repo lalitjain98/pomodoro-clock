e73ac60300b8b731f332814eaebe8b3b67422c1f
Lalit Jain <jain.lalit138@gmail.com>
2019-04-17 18:06:15 +0530

    Updates

---
+++ precache-manifest.4ea49fe7eabf3a88ee4af123c50208a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/pomodoro-clock/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "f7d6974e8686e3adff86",
+    "url": "/pomodoro-clock/static/css/main.b0a89bb6.chunk.css"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9826ad4782b66f957894",
+    "url": "/pomodoro-clock/static/js/2.8bf03afe.chunk.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/pomodoro-clock/static/media/digital-7 (mono).58045dab.ttf"
+  },
+  {
+    "revision": "f7d6974e8686e3adff86",
+    "url": "/pomodoro-clock/static/js/main.015c6dc9.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "9826ad4782b66f957894",
+    "url": "/pomodoro-clock/static/css/2.63d9673d.chunk.css"
+  },
+  {
+    "revision": "10d8447dc1158c687b2252e3fac15a13",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
